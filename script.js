44655174b3aa00081ec1932c5bffe8119e789ee3
debrulima <joaovictorlimaneves@gmail.com>
2022-06-29 09:49:19 -0300

    :bug: imagens

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.querySelector(".hamburguer").addEventListener("click", ()=>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+var btnSubmit = document.querySelector("#btnSubmit");
+
+btnSubmit.addEventListener("click", function(event){
+    event.preventDefault();
+    /*fetch('https://api.sheetmonkey.io/form/h5kAjACMKLKqdUaqb6B8oX');
+
+    var formulario = document.querySelector("#formulario");
+    console.log(formulario.nome.value);
+    console.log(formulario.email.value);
+    console.log(formulario.mensagem.value);*/
+})
